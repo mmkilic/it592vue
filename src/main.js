28e7616bf33ac4a54846a303f93980fadaf11647
mkilic <mkilic.ktu@gmail.com>
2021-08-22 00:35:43 +0300

    Completed Login

--- src/main.js
+++ src/main.js
@@ -2,6 +2,14 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue)
+Vue.use(BootstrapVueIcons)
+
 Vue.config.productionTip = false
 
 new Vue({
---
+++ src/views/Login.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="login">
+    <h1 class="my-5"> Login </h1>
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <login-form />
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import LoginForm from "../components/LoginForm.vue";
+
+export default {
+  name: "Login",
+  components: {
+    LoginForm,
+  },
+};
+</script>
+
+<style scoped>
+</style>
