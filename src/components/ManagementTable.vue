188562133322f0367c6c37cf0ea645a08db89976
mkilic <mkilic.ktu@gmail.com>
2021-08-22 16:56:57 +0300

    started to management page

---
+++ src/components/ManagementTable.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <b-table striped hover small  :items="projects" :fields="tableFields" responsive="sm">
+      <template #cell(Action)="popup">
+        <b-button size="sm" @click="popup.toggleDetails" class="mr-2">Edit</b-button>
+      </template>
+    </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["projects"],
+  data() {
+    return {
+      tableFields: [
+        {
+          key: "id",
+          sortable: true,
+        },
+        {
+          key: "projectNumber.number",
+          label: "Number",
+          sortable: true,
+        },
+        {
+          key: "projectInfo.powerOnan",
+          label: "ONAN",
+          sortable: true,
+        },
+        {
+          key: "projectInfo.powerOnaf",
+          label: "ONAF",
+          sortable: true,
+        },
+        {
+          key: "projectInfo.lowVoltage",
+          label: "HV",
+          sortable: false,
+        },
+        {
+          key: "projectInfo.highVoltage",
+          label: "LV",
+          sortable: false,
+        },
+        {
+          key: "projectInfo.projectManger.fullName",
+          label: "PM",
+          sortable: true,
+        },
+        {
+          key: "projectInfo.createDate",
+          label: "Create Date",
+          sortable: true,
+        },
+        {
+          key: "projectInfo.productType",
+          label: "Type",
+          sortable: false,
+        },
+        {
+          key: "Action",
+          label: "Action",
+          sortable: false,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ src/views/Management.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="management">
+    <h1 class="my-2">New Project</h1>
+    <div class="row">
+      <div class="col-md-3"></div>
+      <div class="col-md-6">
+        <management-form />
+      </div>
+      <div class="col-md-3"></div>
+    </div>
+    <hr>
+    <h1 class="my-2">Project List</h1>
+    <div class="row">
+      <div class="col-md-2"></div>
+      <div class="col-md-6">
+        <management-table :projects=projects></management-table>
+      </div>
+      <div class="col-md-2"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+import ManagementForm from "../components/ManagementForm.vue";
+import ManagementTable from '../components/ManagementTable.vue';
+import axios from "axios";
+
+export default {
+  components: { ManagementForm, ManagementTable },
+  data() {
+    return {
+      projects: null,
+    };
+  },
+  created: async function () {
+    await axios.get("http://localhost:8081/api/prj")
+    .then(response => {
+      this.projects = response.data
+    })
+    .catch(e => {
+      console.log(e)
+    })
+  },
+};
+</script>
+
+<style>
+</style>
