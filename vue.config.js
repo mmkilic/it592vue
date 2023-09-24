e7bb2f858cde0d1713c91c213b74d56c4fb28e5e
Mehmet <mkilic.ktu@gmail.com>
2023-09-24 15:32:52 +0300

    home page url has been set for deployment

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,5 @@
+publicPath: process.env.NODE_ENV === 'production'
 module.exports = {
-  lintOnSave: false
+  lintOnSave: false,
+  publicPath: process.env.NODE_ENV === 'production' ? '/it592vue/' : '/'
 };
